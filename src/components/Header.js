1e350e07622041bc6cd4d44bb85378a1a72f3c13
Bruno Braga <brunobragaw8t@gmail.com>
2021-12-29 12:25:16 +0000

    Fix HTML attributes for JSX

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,27 +11,27 @@ export const Header = () => {
         <h1 className="h2 mb-0">Task Manager</h1>
 
         <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#create-task">
-          <i class="bi bi-plus-square-dotted"></i>
+          <i className="bi bi-plus-square-dotted"></i>
         </button>
 
-        <div className="modal fade" id="create-task" tabindex="-1" aria-labelledby="create-task-label" aria-hidden="true">
+        <div className="modal fade" id="create-task" tabIndex="-1" aria-labelledby="create-task-label" aria-hidden="true">
           <div className="modal-dialog">
             <form className="modal-content">
               <div className="modal-header">
-                <h5 class="modal-title" id="create-task-label">Create task</h5>
+                <h5 className="modal-title" id="create-task-label">Create task</h5>
 
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
 
               <div className="modal-body">
                 <div className="mb-3">
-                  <label htmlFor="title" class="form-label">Title</label>
+                  <label htmlFor="title" className="form-label">Title</label>
 
                   <input type="text" className="form-control" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
                 </div>
 
                 <div className="mb-3">
-                  <label htmlFor="due-date" class="form-label">Due date</label>
+                  <label htmlFor="due-date" className="form-label">Due date</label>
 
                   <DatePicker
                     className="form-control"
@@ -43,10 +43,10 @@ export const Header = () => {
                 </div>
               </div>
 
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
 
-                <button type="submit" class="btn btn-primary">Create</button>
+                <button type="submit" className="btn btn-primary">Create</button>
               </div>
             </form>
           </div>
